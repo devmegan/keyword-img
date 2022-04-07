8760f253778f04cbd6cfc63a9133face4594264e
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-07 08:21:01 +0000

    import components to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import Hero from './component/Hero';
+import Nav from './component/Nav';
+import { THEME } from './Config';
+
 export default function App() {
   return (
-    <div className="hero min-h-screen bg-base-200" data-theme="forest">
-      <div className="hero-content text-center">
-        <div className="max-w-screen-md">
-          <h1 className="text-5xl font-bold"><a href="https://github.com/devmegan/" className="hover:underline">devmegan/react-template</a></h1>
-          <p className="py-6">A <a href="https://reactjs.org/" className="link link-primary">React</a> template with <a className="link link-primary" href="https://tailwindcss.com/">tailwindcss</a> and <a className="link link-primary">daisyUI</a> configured.</p>
-        </div>
-      </div>
+    <div className="min-h-screen bg-accent" data-theme={THEME}>
+        <Nav />
+        <Hero />
     </div>
   );
 }
