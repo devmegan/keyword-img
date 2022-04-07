2394a76d32ca466c03821e885650f6468dcbfdb8
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-07 08:20:34 +0000

    add description to config

--- src/Config.js
+++ src/Config.js
@@ -3,6 +3,11 @@ const HREFS = {
     repo: 'https://github.com/devmegan/keyword-img',
   }
   
-const THEME = "pastel";
+const THEME = "garden";
 
-export { HREFS, THEME };
+const DESCRIPTION = {
+    header: 'Enter a keyword...', 
+    para: 'To receive images associated with it'
+}
+
+export { HREFS, THEME, DESCRIPTION };
