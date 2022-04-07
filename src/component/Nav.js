fabeb3546aa85c21fc1b060f6933162cd5d76526
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-07 08:21:45 +0000

    add nav

---
+++ src/component/Nav.js
@@ -0,0 +1,18 @@
+import { HREFS } from '../Config';
+
+export default function Nav() {
+    return (
+        <div className="navbar bg-primary text-white">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl" href={HREFS.app}>keyword-img</a>
+            </div>
+            {HREFS.repo && (
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal p-0">
+                        <li><a href={HREFS.repo} target="_blank">Repo</a></li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    )
+}
